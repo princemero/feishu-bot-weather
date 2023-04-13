0e05041622d007110ff0ac820033345ed0815298
princemero <52058661+princemero@users.noreply.github.com>
2023-04-13 12:31:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def send_weather():
 if __name__ == "__main__":
     # 构造定时任务
     scheduler = BlockingScheduler()
-    scheduler.add_job(send_weather, "cron", hour = 7 ,minute =45 )
+    scheduler.add_job(send_weather, "cron", day_of_week='mon-fri', hour=7, minute=45)
 
     # 启动定时任务
     scheduler.start()
