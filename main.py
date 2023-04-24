05cbe617e504289c21178d43a9c54ae67c5f16e6
Kano <52058661+princemero@users.noreply.github.com>
2023-04-24 08:41:59 +0800

    Update main.py
    增加其他天气信息

--- main.py
+++ main.py
@@ -22,7 +22,9 @@ def send_weather():
     data = response.json()["results"][0]["daily"][0]
 
     # 构造要推送的消息
-    message = f"上海今日天气：{data['text_day']}，最高气温：{data['high']}℃，最低气温：{data['low']}℃。"
+    message = f"上海今日天气：{data['text_day']}，\n最高气温：{data['high']}℃，\n最低气温：{data['low']}℃，" \
+              f"\n风向：{data['wind_direction']}，\n风速：{data['wind_speed']}，\n风力等级：{data['wind_scale']}，" \
+              f"\n降水量：{data['rainfall']}mm，\n降水概率：{data['precip']}%，\n相对湿度：{data['humidity']}% "
 
     # 推送消息到飞书机器人
     headers = {
